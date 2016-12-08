8b49f9a4bdca2df81e8a8a9ccde79764fef8814c
Roja07 <160120@yses.ac.jp>
2016-12-08 10:26:35 +0900

    ShowByteImage を　追加しました。

---
+++ 20161208/Aloha.java
@@ -0,0 +1,21 @@
+public class Aloha{
+	public static void main(String[] args) {
+		//for (int i=0; i<30; i++) {
+			//System.out.println("ALOHA");
+		//int i=0;
+		//while(i<30){
+		//	System.out.println("ALOHA");
+		//	i++;
+		while(true){
+		System.out.print("入力してください。");
+		String data=new java.util.Scanner(System.in).nextLine();
+		System.out.println(data);
+
+		if (data.equals("")) {
+			break;
+			}
+		}
+		}
+	
+}
+
