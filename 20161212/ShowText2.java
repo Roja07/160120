1bb0dcc5bc86f8ac4cbb8aebe543684035751775
Roja07 <160120@yses.ac.jp>
2016-12-12 14:23:41 +0900

    まだ　です。

---
+++ 20161212/ShowText2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class ShowText2{
+   public static void main(String[] args) {
+
+String filename;
+FileReader;
+BufferedReader;
+int n=0;
+
+try{
+
+	while(){
+		System.out.println(data);
+		n++;
+
+	}
+
+}catch(IOException e){
+
+}
