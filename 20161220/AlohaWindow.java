6517f1d198fe73ccfa6372a6d9cc5df1d46210a6
Roja07 <160120@yses.ac.jp>
2016-12-20 11:37:14 +0900

    AlohaWindow.java Added.

--- 20161220/AlohaWindow.java
+++ 20161220/AlohaWindow.java
@@ -5,8 +5,22 @@ public class AlohaWindow{
 	public static void main(String[] args) {
 		JFrame frame=new JFrame();
 
-		frame.setSize(200,200);
+		frame.setSize(400,200);
+
+		frame.setTitle("アオキ！");
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.setLayout(new FlowLayout());
+
+		JLabel label=new JLabel("HELLO");
+		Container ctnr=frame.getContentPane();
+		ctnr.add(label);
+
+		JButton button=new JButton("Click Me!");
+		ctnr.add(button);
 
 		frame.setVisible(true);
+
 	}
 } 
