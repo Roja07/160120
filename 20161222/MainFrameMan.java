1aa0f2eae108c0d3bed919730f81a6decd7a4656
Roja07 <160120@yses.ac.jp>
2016-12-22 09:54:06 +0900

    FrameMan is here.

---
+++ 20161222/MainFrameMan.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class MainFrameMan{
+	public static void main(String[] args) {
+		
+		FrameMan fm=new FrameMan();
+
+	}
+}
+class FrameMan{
+
+	JFrame frame;
+
+	 public FrameMan(){
+	 	System.out.println("ALOHA3!");
+	 }
+	 	
+	 
+}
